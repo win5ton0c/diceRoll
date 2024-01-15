00a637b89cdfad21a8924bea4ed8192b0a60b100
wiston <wistonlee32@gmail.com>
2024-01-15 15:22:44 +0000

    Update dice.css and dice.html, add functionality to roll multiple dice

--- dice.js
+++ dice.js
@@ -4,10 +4,17 @@
     const numOfDice = document.getElementById('numOfDice').value;
     const diceResult = document.getElementById('diceResult');
     const diceImages = document.getElementById('diceImages');
+    const values =[];
+    const images = [];
 
-    output = Math.floor(Math.random() * 6) + 1;
-    console.log(`${output}`);
+    for(let i = 0; i<numOfDice ; i++){
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src ="dice_images/dice${value}.png" alt = "Dice ${value}">`)
 
+    }
 
+    diceResult.textContent = `dice: ${values.join(' ,')}`;
+    diceImages.innerHTML = images.join(' ');
  }
 
